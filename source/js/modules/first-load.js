2059d0f45460a6c95107b4b46dc6e5702400c4ce
nicks <k.baranov@redcollar.ru>
2022-07-27 13:14:55 +0300

    Добавляем анимацию появления шапки

---
+++ source/js/modules/first-load.js
@@ -0,0 +1,15 @@
+export default () => {
+  // const header = document.querySelector(`.header`);
+  const socialBlock = document.querySelector(`.social-block`);
+
+  window.addEventListener(`load`, () => {
+    document.body.classList.add(`page-loaded`);
+    socialBlock.classList.add(`loaded`);
+    // header.classList.add(`loaded`);
+
+    setTimeout(()=> {
+      socialBlock.classList.remove(`loaded`);
+      // header.classList.remove(`loaded`);
+    }, 500);
+  });
+};
